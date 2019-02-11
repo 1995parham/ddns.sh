8239d446f24ea7e537e55d34b6e6f3794deaf25d
Parham Alvani <parham.alvani@gmail.com>
2019-02-12 00:00:48 +0330

    Print time in each iteration.

--- ddns.sh
+++ ddns.sh
@@ -8,6 +8,7 @@
 # [] Created By : Parham Alvani <parham.alvani@gmail.com>
 # =======================================
 while true; do
+        date
         curl -#L http://ifconfig.io/ip | tee ip.address
         sleep 3600
 done
