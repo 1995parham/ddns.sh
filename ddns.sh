11bf5cdf40008c6b5b5130233b30f2c73bc28416
Parham Alvani <parham.alvani@gmail.com>
2019-02-12 00:00:26 +0330

    Adds basic script for ddns.sh.

---
+++ ddns.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# In The Name of God
+# ========================================
+# [] File Name : ddns.sh
+#
+# [] Creation Date : 11-02-2019
+#
+# [] Created By : Parham Alvani <parham.alvani@gmail.com>
+# =======================================
+while true; do
+        curl -#L http://ifconfig.io/ip | tee ip.address
+        sleep 3600
+done
